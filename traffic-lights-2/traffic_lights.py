17530f744a3ead6cfe1581d21535fd3bdb0e3339
Luke Hackett <lhackett@live.com>
2013-07-27 17:55:43 +0000

    Addition of pedestrian functionality

--- traffic-lights-2/traffic_lights.py
+++ traffic-lights-2/traffic_lights.py
@@ -2,13 +2,13 @@
 #
 # Copyright 2013 Luke Hackett
 # https://github.com/LukeHackett
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,29 +18,76 @@
 import time
 import RPi.GPIO as GPIO
 
-# LED constants
+# Traffic LED constants
 RED_LED = 18
 AMB_LED = 23
 GRE_LED = 24
+
+# Pedestrian LED constants
+PED_RED_LED = 25
+PED_GRE_LED = 8
+
+# Pedestrian button constants
+PED_BUTTON = 7
+PED_WAITING = False
 
 # Initialise the board
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
-# Initialise the pins
+# Initialise the output pins
 GPIO.setup(RED_LED, GPIO.OUT)
 GPIO.setup(AMB_LED, GPIO.OUT)
 GPIO.setup(GRE_LED, GPIO.OUT)
+GPIO.setup(PED_RED_LED, GPIO.OUT)
+GPIO.setup(PED_GRE_LED, GPIO.OUT)
+
+# Initialise the input pins
+GPIO.setup(PED_BUTTON, GPIO.IN)
+
+# Crssing request function
+def cross_request(channel):
+	global PED_WAITING
+	PED_WAITING = True
+
+# Detects a button press, executing the callback upon a new thread
+GPIO.add_event_detect(PED_BUTTON, GPIO.RISING, callback=cross_request, bouncetime=200)
+
 
 try:
 	while(True):
-		# Force cars to stop (RED)
+		# Force cars and pedestrians to stop (RED)
 		GPIO.output(RED_LED, GPIO.HIGH)
+		GPIO.output(PED_RED_LED, GPIO.HIGH)
 		time.sleep(2)
 		
-		# Allow cars to start moving (RED/AMBER)
-		GPIO.output(AMB_LED, GPIO.HIGH)
-		time.sleep(2)
+		if PED_WAITING:
+			# Allows the pedestrian to cross for 5 seconds
+			GPIO.output(PED_RED_LED, GPIO.LOW)
+			GPIO.output(PED_GRE_LED, GPIO.HIGH)
+			time.sleep(5)
+			
+			# Prevent all pedestrians from crossing
+			GPIO.output(PED_GRE_LED, GPIO.LOW)
+			GPIO.output(PED_RED_LED, GPIO.HIGH)
+			
+			# Turn off the traffic RED light
+			GPIO.output(RED_LED, GPIO.LOW)			
+
+			# Flash the amber light
+			for i in range(0, 4):
+				GPIO.output(AMB_LED, GPIO.HIGH)
+				time.sleep(0.5)
+				GPIO.output(AMB_LED, GPIO.LOW)
+				time.sleep(0.5)
+
+			# Clear the waiting flag
+			PED_WAITING = False 
+
+		else:
+			# Allow cars to start moving (RED/AMBER)
+			GPIO.output(AMB_LED, GPIO.HIGH)
+			time.sleep(2)
 		
 		# All cars can go (GREEN) 
 		GPIO.output(RED_LED, GPIO.LOW)
@@ -63,6 +110,8 @@ except (KeyboardInterrupt, SystemExit):
 	GPIO.output(RED_LED, GPIO.LOW)
 	GPIO.output(AMB_LED, GPIO.LOW)
 	GPIO.output(GRE_LED, GPIO.LOW)
+	GPIO.output(PED_RED_LED, GPIO.LOW)
+	GPIO.output(PED_GRE_LED, GPIO.LOW)
 	
 	# Reset the PINs
 	GPIO.cleanup()
